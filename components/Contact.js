397ec510d901e1a99c6fb3fa6c599dc2298ea363
rajibcsit <18192203021@cse.bubt.edu.bd>
2023-02-20 15:33:53 +0600

    Create Contact component and import index page

---
+++ components/Contact.js
@@ -0,0 +1,88 @@
+import Image from "next/image";
+import contactImage from "/public/images/contact.svg";
+function Contact() {
+  return (
+    <div>
+      <>
+        <section id="contact" className="contact_area relative pt-18 pb-120">
+          <div className="contact_image flex items-center justify-end">
+            <div className="image lg:pr-13">
+              <Image src={contactImage} alt="contact-image" />
+            </div>
+          </div>
+
+          <div className="container">
+            <div className="row justify-end">
+              <div className="w-full lg:w-1/2">
+                <div className="contact_wrapper mt-11">
+                  <div className="section_title pb-4">
+                    <h5 className="sub_title">Contact</h5>
+                    <h4 className="main_title">Get In Touch</h4>
+                  </div>
+
+                  <div className="contact_form">
+                    <form id="contact-form" action="" method="POST">
+                      <div className="row">
+                        <div className="w-full md:w-1/2">
+                          <div className="mx-3">
+                            <div className="single_form mt-8">
+                              <input
+                                name="name"
+                                id="name"
+                                type="text"
+                                placeholder="Name"
+                                className="w-full rounded-md py-4 px-6 border border-solid border-body-color"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="w-full md:w-1/2">
+                          <div className="mx-3">
+                            <div className="single_form mt-8">
+                              <input
+                                name="email"
+                                id="email"
+                                type="email"
+                                placeholder="Email"
+                                className="w-full rounded-md py-4 px-6 border border-solid border-body-color"
+                              />
+                            </div>
+                          </div>
+                        </div>
+                        <div className="w-full">
+                          <div className="mx-3">
+                            <div className="single_form mt-8">
+                              <textarea
+                                name="message"
+                                id="message"
+                                placeholder="Message"
+                                rows="5"
+                                className="w-full rounded-md py-4 px-6 border border-solid border-body-color resize-none"
+                              ></textarea>
+                            </div>
+                          </div>
+                        </div>
+                        <p className="form-message mx-3"></p>
+                        <div className="w-full">
+                          <div className="mx-3">
+                            <div className="single_form mt-8">
+                              <button type="submit" className="main-btn contact-btn">
+                                Submit
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    </div>
+  );
+}
+
+export default Contact;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
-import Link from "next/link";
-
-import contactImage from "/public/images/contact.svg";
 
 import Home from "@/components/Home";
 import About from "@/components/About";
@@ -10,6 +6,7 @@ import Services from "@/components/Services";
 import Projects from "@/components/Projects";
 import Pricing from "@/components/Pricing";
 import Team from "@/components/Team";
+import Contact from "@/components/Contact";
 
 export default function index() {
   return (
@@ -40,115 +37,19 @@ export default function index() {
           </div>
         </div>
 
-        {/*--====== PRELOADER PART ENDS ======--*/}
-
-        {/*--------- HEADER PART Start------------ */}
         <Home />
-        {/*--------- HEADER PART End------------ */}
 
-        {/*--====== ABOUT PART START ======--*/}
         <About />
-        {/*--====== ABOUT PART ENDS ======--*/}
 
-        {/*--====== SERVICES PART START ======--*/}
         <Services />
 
-        {/*--====== SERVICES PART ENDS ======--*/}
-
-        {/*-------------------------- PROJECTS PART START------------ */}
         <Projects />
-        {/*-------------------------- PROJECTS PART END------------ */}
 
-        {/*--====== PRICING PLAN PART START ======--*/}
         <Pricing />
-        {/*--====== PRICING PLAN PART ENDS ======--*/}
 
-        {/*--====== TEAM PART START ======---------*/}
         <Team />
-        {/*--====== TEAM PART ENDS ======---------*/}
 
-        {/*--====== CONTACT PART START ======---------*/}
-        <section id="contact" className="contact_area relative pt-18 pb-120">
-          <div className="contact_image flex items-center justify-end">
-            <div className="image lg:pr-13">
-              <Image src={contactImage} alt="contact-image" />
-            </div>
-          </div>
-
-          <div className="container">
-            <div className="row justify-end">
-              <div className="w-full lg:w-1/2">
-                <div className="contact_wrapper mt-11">
-                  <div className="section_title pb-4">
-                    <h5 className="sub_title">Contact</h5>
-                    <h4 className="main_title">Get In Touch</h4>
-                  </div>
-
-                  <div className="contact_form">
-                    <form id="contact-form" action="" method="POST">
-                      <div className="row">
-                        <div className="w-full md:w-1/2">
-                          <div className="mx-3">
-                            <div className="single_form mt-8">
-                              <input
-                                name="name"
-                                id="name"
-                                type="text"
-                                placeholder="Name"
-                                className="w-full rounded-md py-4 px-6 border border-solid border-body-color"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                        <div className="w-full md:w-1/2">
-                          <div className="mx-3">
-                            <div className="single_form mt-8">
-                              <input
-                                name="email"
-                                id="email"
-                                type="email"
-                                placeholder="Email"
-                                className="w-full rounded-md py-4 px-6 border border-solid border-body-color"
-                              />
-                            </div>
-                          </div>
-                        </div>
-                        <div className="w-full">
-                          <div className="mx-3">
-                            <div className="single_form mt-8">
-                              <textarea
-                                name="message"
-                                id="message"
-                                placeholder="Message"
-                                rows="5"
-                                className="w-full rounded-md py-4 px-6 border border-solid border-body-color resize-none"
-                              ></textarea>
-                            </div>
-                          </div>
-                        </div>
-                        <p className="form-message mx-3"></p>
-                        <div className="w-full">
-                          <div className="mx-3">
-                            <div className="single_form mt-8">
-                              <button type="submit" className="main-btn contact-btn">
-                                Submit
-                              </button>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-        {/*--====== CONTACT PART ENDS ======---------*/}
-
-        {/*--====== FOOTER PART START ======---------*/}
-
-        {/*--====== FOOTER PART ENDS ======---------*/}
+        <Contact />
       </div>
     </>
   );
