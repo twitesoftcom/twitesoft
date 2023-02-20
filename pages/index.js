9ec4864734e185f49a8675756f71e07f41a146f2
rajibcsit <18192203021@cse.bubt.edu.bd>
2023-02-20 14:54:29 +0600

    Create about component and import index page

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,6 @@ import shapeImage10 from "/public/images/shape/shape-3.svg";
 import shapeImage11 from "/public/images/shape/shape-1.svg";
 
 import headerImage from "/public/images/header-image.svg";
-import aboutImage from "/public/images/about.svg";
 
 import projectPos from "/public/images/pos.jpg";
 import teamImg1 from "/public/images/team/team1.jpg";
@@ -25,6 +24,8 @@ import teamImg4 from "/public/images/team/team4.jpg";
 import teamImg5 from "/public/images/team/team5.webp";
 
 import contactImage from "/public/images/contact.svg";
+
+import About from "@/components/About";
 
 export default function Home() {
   return (
@@ -131,170 +132,8 @@ export default function Home() {
 
         {/*--------- HEADER PART End------------ */}
 
-        {/*--====== SERVICES PART ENDS ======--*/}
-
-        <section className="services_area pt-120" id="about">
-          <div className="container">
-            <div className="row justify-center">
-              <div className="w-full lg:w-1/2">
-                <div className="section_title text-center pb-6">
-                  <h5 className="sub_title">About</h5>
-                  <h4 className="main_title">Work Process</h4>
-                </div>
-              </div>
-            </div>
-            <div className="row justify-center">
-              <div className="w-full sm:w-10/12 md:w-6/12 lg:w-4/12">
-                <div className="single_services text-center mt-8 mx-3">
-                  <div className="services_icon">
-                    <i className="lni lni-write"></i>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="94"
-                      height="92"
-                      viewBox="0 0 94 92"
-                    >
-                      <path
-                        className="services_shape"
-                        id="Polygon_12"
-                        data-name="Polygon 12"
-                        d="M42.212,2.315a11,11,0,0,1,9.576,0l28.138,13.6a11,11,0,0,1,5.938,7.465L92.83,54.018A11,11,0,0,1,90.717,63.3L71.22,87.842A11,11,0,0,1,62.607,92H31.393a11,11,0,0,1-8.613-4.158L3.283,63.3A11,11,0,0,1,1.17,54.018L8.136,23.383a11,11,0,0,1,5.938-7.465Z"
-                      />
-                    </svg>
-                  </div>
-                  <div className="services_content mt-5">
-                    <h3 className="services_title text-black font-semibold text-xl md:text-3xl">
-                      Research
-                    </h3>
-                    <p className="mt-4">
-                      Lorem ipsum dolor sit amet, consetetur sadi aliquyam erat, sed diam voluptua.
-                      At vero eos accusam et justo duo dolores{" "}
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="w-full sm:w-10/12 md:w-6/12 lg:w-4/12">
-                <div className="single_services text-center mt-8 mx-3">
-                  <div className="services_icon">
-                    <i className="lni lni-bulb"></i>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="94"
-                      height="92"
-                      viewBox="0 0 94 92"
-                    >
-                      <path
-                        className="services_shape"
-                        id="Polygon_12"
-                        data-name="Polygon 12"
-                        d="M42.212,2.315a11,11,0,0,1,9.576,0l28.138,13.6a11,11,0,0,1,5.938,7.465L92.83,54.018A11,11,0,0,1,90.717,63.3L71.22,87.842A11,11,0,0,1,62.607,92H31.393a11,11,0,0,1-8.613-4.158L3.283,63.3A11,11,0,0,1,1.17,54.018L8.136,23.383a11,11,0,0,1,5.938-7.465Z"
-                      />
-                    </svg>
-                  </div>
-                  <div className="services_content mt-5">
-                    <h3 className="services_title text-black font-semibold text-xl md:text-3xl">
-                      Prototype
-                    </h3>
-                    <p className="mt-4">
-                      Lorem ipsum dolor sit amet, consetetur sadi aliquyam erat, sed diam voluptua.
-                      At vero eos accusam et justo duo dolores{" "}
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="w-full sm:w-10/12 md:w-6/12 lg:w-4/12">
-                <div className="single_services text-center mt-8 mx-3">
-                  <div className="services_icon">
-                    <i className="lni lni-checkmark-circle"></i>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="94"
-                      height="92"
-                      viewBox="0 0 94 92"
-                    >
-                      <path
-                        className="services_shape"
-                        id="Polygon_12"
-                        data-name="Polygon 12"
-                        d="M42.212,2.315a11,11,0,0,1,9.576,0l28.138,13.6a11,11,0,0,1,5.938,7.465L92.83,54.018A11,11,0,0,1,90.717,63.3L71.22,87.842A11,11,0,0,1,62.607,92H31.393a11,11,0,0,1-8.613-4.158L3.283,63.3A11,11,0,0,1,1.17,54.018L8.136,23.383a11,11,0,0,1,5.938-7.465Z"
-                      />
-                    </svg>
-                  </div>
-                  <div className="services_content mt-5">
-                    <h3 className="services_title text-black font-semibold text-xl md:text-3xl">
-                      Build
-                    </h3>
-                    <p className="mt-4">
-                      Lorem ipsum dolor sit amet, consetetur sadi aliquyam erat, sed diam voluptua.
-                      At vero eos accusam et justo duo dolores{" "}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
-        {/*--====== SERVICES PART ENDS ======--*/}
-
         {/*--====== ABOUT PART START ======--*/}
-
-        <section id="why" className="about_area pt-120 relative">
-          <div className="about_image flex items-end justify-end">
-            <div className="image lg:pr-13">
-              <Image src={aboutImage} alt="about image" />
-            </div>
-          </div>
-          <div className="container">
-            <div className="row justify-end">
-              <div className="w-full lg:w-1/2">
-                <div className="about_content mx-4 pt-11 lg:pt-15 lg:pb-15">
-                  <div className="section_title pb-9">
-                    <h5 className="sub_title">Why Choose Us</h5>
-                    <h4 className="main_title">Your Goal is Our Achievement</h4>
-                  </div>
-                  <p>
-                    In <span className="text-emerald-500">TwiteSoft</span> we follow a straight,
-                    relaxing and transparent work life that uses an agile approach and not the "old
-                    hierarchies". We want to break down the silos, share all we can and empower all
-                    of us to be the best we can be. There is a word for this organisation method,
-                    and it is Holacracy.
-                  </p>
-                  <div className="text-xl font-semibold mt-6">
-                    <h4>We Like:</h4>
-                  </div>
-                  <ul className="about_list pt-3">
-                    <li className="flex mt-5">
-                      <div className="about_check">
-                        <i className="lni lni-checkmark-circle"></i>
-                      </div>
-                      <div className="about_list_content pl-5 pr-2">
-                        <p>Open team based work life</p>
-                      </div>
-                    </li>
-                    <li className="flex mt-5">
-                      <div className="about_check">
-                        <i className="lni lni-checkmark-circle"></i>
-                      </div>
-                      <div className="about_list_content pl-5 pr-2">
-                        <p>Self-thinking agile teams</p>
-                      </div>
-                    </li>
-                    <li className="flex mt-5">
-                      <div className="about_check">
-                        <i className="lni lni-checkmark-circle"></i>
-                      </div>
-                      <div className="about_list_content pl-5 pr-2">
-                        <p>A trust based open mindset</p>
-                      </div>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-
+        <About />
         {/*--====== ABOUT PART ENDS ======--*/}
 
         {/*--====== SERVICES PART START ======--*/}
