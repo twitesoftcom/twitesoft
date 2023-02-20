8c058f7b296f7dd32e8a7fb4df2e24ba5132b3ea
rajibcsit <18192203021@cse.bubt.edu.bd>
2023-02-20 13:57:00 +0600

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '@/styles/globals.css'
+import "@/styles/animate.css";
+import "@/styles/tiny-slider.css";
+import "@/styles/fonts/lineicons/font-css/LineIcons.css";
+import "@/styles/tailwindcss.css";
+// import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
