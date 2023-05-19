5a9b5bb4888aa6b9bce65804728d191c94c80747
Al-Mamun Sarkar <msarkar.cse@gmail.com>
2023-05-19 23:51:37 +0600

    Update content

--- components/Services.js
+++ components/Services.js
@@ -28,7 +28,7 @@ function Services() {
                         clipRule="evenodd"
                       >
                         <path
-                          d="M24 1v16.981h-13l-7 5.02v-5.02h-4v-16.981h24zm-2 15v-12.999h-20v12.999h4v3.105l4.357-3.105h11.643zm-4-9.715l-6.622 7.715-4.378-3.852 1.319-1.489 2.879 2.519 5.327-6.178 1.475 1.285z"
+                          d="M24 8.2c0-.318-.126-.623-.351-.849-.226-.225-.531-.351-.849-.351h-6.6c-.318 0-.623.126-.849.351-.225.226-.351.531-.351.849v13.6c0 .318.126.623.351.849.226.225.531.351.849.351h6.6c.318 0 .623-.126.849-.351.225-.226.351-.531.351-.849v-13.6zm-11 14.8h-8l2.599-3h5.401v3zm6.5-1c-.553 0-1-.448-1-1s.447-1 1-1c.552 0 .999.448.999 1s-.447 1-.999 1zm3.5-3v-9.024h-7v9.024h7zm-2-14h-2v-2h-17v13h11v2h-13v-17h21v4zm-.5 4c.276 0 .5-.224.5-.5s-.224-.5-.5-.5h-2c-.276 0-.5.224-.5.5s.224.5.5.5h2z"
                           className="service_svg_icon"
                         />
                       </svg>
@@ -74,7 +74,7 @@ function Services() {
                         clipRule="evenodd"
                       >
                         <path
-                          d="M24 8.2c0-.318-.126-.623-.351-.849-.226-.225-.531-.351-.849-.351h-6.6c-.318 0-.623.126-.849.351-.225.226-.351.531-.351.849v13.6c0 .318.126.623.351.849.226.225.531.351.849.351h6.6c.318 0 .623-.126.849-.351.225-.226.351-.531.351-.849v-13.6zm-11 14.8h-8l2.599-3h5.401v3zm6.5-1c-.553 0-1-.448-1-1s.447-1 1-1c.552 0 .999.448.999 1s-.447 1-.999 1zm3.5-3v-9.024h-7v9.024h7zm-2-14h-2v-2h-17v13h11v2h-13v-17h21v4zm-.5 4c.276 0 .5-.224.5-.5s-.224-.5-.5-.5h-2c-.276 0-.5.224-.5.5s.224.5.5.5h2z"
+                          d="M24 1v16.981h-13l-7 5.02v-5.02h-4v-16.981h24zm-2 15v-12.999h-20v12.999h4v3.105l4.357-3.105h11.643zm-4-9.715l-6.622 7.715-4.378-3.852 1.319-1.489 2.879 2.519 5.327-6.178 1.475 1.285z"
                           className="service_svg_icon"
                         />
                       </svg>
@@ -283,9 +283,12 @@ function Services() {
                       Lead Generation
                     </h3>
                     <p className="mt-4">
-                      Fuel your business growth with our lead generation
+                      {/* Fuel your business growth with our lead generation
                       service, providing a steady stream of qualified prospects
-                      and potential customers for your products or services.
+                      and potential customers for your products or services. */}
+                      Our lead generation service is designed to fuel your
+                      business growth by delivering a consistent flow potential
+                      customers.
                     </p>
                   </div>
                 </div>
